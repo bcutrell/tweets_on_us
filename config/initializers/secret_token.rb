aa66e6642761e5a46d9c08fb24a632ebac1562ac
Ben Cutrell <bcutrell13@gmail.com>
2013-10-07 17:14:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetsOnUs::Application.config.secret_key_base = 'd96c31035d761defc7a15ee3c717f44fbf57698c54f05bb7c44d10178a24e7409c4bead84127fa1a604053a0f059780ae7b1743e860f137658ea54028dde68d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TweetsOnUs::Application.config.session_store :cookie_store, key: '_tweets_on_us_session'
