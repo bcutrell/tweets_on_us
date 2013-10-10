8da30a42f3311952d548dc2f0f6c0f325427093d
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-10-10 16:34:05 -0400

    Admin can add a tweeter handle.

---
+++ spec/features/admin_creates_tweeter_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+feature 'admin creates a tweeter', %Q{
+  As the admin
+  I can add tweeters
+  So that I can follow their tweets
+} do 
+
+  # Acceptance Criteria:
+  # * Admin can add tweeters
+  # *Admin can remove tweeters
+
+before (:each) do 
+  @tweet = FactoryGirl.create(:tweeter) 
+  @prev_count = Tweeter.count
+end
+
+let!(:user) {FactoryGirl.create(:administrator)}
+
+
+  scenario 'admin successfully creates a tweeter' do
+    user = FactoryGirl.create(:administrator)
+
+    login_with_oauth
+    click_on 'Admin'
+    within('.sidebar-nav') do
+      page.click_link('Tweeters')
+    end
+    click_on 'Add new'
+    fill_in 'Handle', with: 'wannabefro'
+    click_on 'Save'
+
+    expect(page).to have_content("Tweeter successfully created")
+    expect(page).to have_content("wannabefro")
+    expect(Tweeter.count).to eql(@prev_count + 1)
+  end
+
+  scenario 'admin fails to create a tweeter' do
+   user = FactoryGirl.create(:administrator)
+
+    login_with_oauth
+    click_on 'Admin'
+    within('.sidebar-nav') do
+      page.click_link('Tweeters')
+    end
+    click_on 'Add new'
+    click_on 'Save'
+
+    expect(page).to_not have_content("Tweeter successfully created")
+    expect(Tweeter.count).to eql(@prev_count)
+  end
+
+  scenario 'admin can\'t create the same tweeter twice' do 
+    user = FactoryGirl.create(:administrator)
+
+    login_with_oauth
+    click_link 'Admin'
+    within('.sidebar-nav') do 
+      page.click_link('Tweeters')
+    end
+    click_on 'Add new'
+    fill_in 'Handle', with: @tweet.handle
+    click_on 'Save'
+
+    expect(page).to have_content("Tweeter failed to be created")
+    expect(Tweeter.count).to eql(@prev_count)
+  end
+end
