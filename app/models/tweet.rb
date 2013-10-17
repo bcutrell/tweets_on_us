7b2575f836a96eb604d50f729896fe4fb11e9e42
kdurr <kristina.l.durr@gmail.com>
2013-10-17 16:11:08 -0400

    clean up tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,21 +10,6 @@ class Tweet < ActiveRecord::Base
     def from_today
       Tweet.where(tweet_date: Date.today)
     end
-
-    def tweeter_tag
-      # tweeters = []
-      # Tweet.from_today.each do |tweet|
-      #   if !tweeters.include?(tweet.tweeter)
-      #     tweeters << tweet.tweeter
-      #   end
-      # end
-      
-    end
-
-    def time_range
-      start_date = Date.today - 30
-      end_time = Date.today
-
-    end
+    
   end
 end
