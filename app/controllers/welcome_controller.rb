3e12e139e7087e95decd28d3109d75543c2f0744
Ben Cutrell <bcutrell13@gmail.com>
2013-10-19 15:02:53 -0400

    changes background color

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ApplicationController
   def index
     if params[:handle].present?
       @tweeter = Tweeter.where(id: params[:handle][:tweeter_id]).first
+    elsif params[:tag].present?
+      @group = Group.where(id: params[:tag][:group_id]).first
     end
     @group_tweets = Group.tweeter_tweets_count
   end
